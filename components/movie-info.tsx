af974e915351a67918aad87f891c9933e2f2e7d6
tmbb-japan <woo787@naver.com>
2024-03-25 08:11:53 +0900

    feat : Dynamic metaData

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,7 +1,7 @@
 import potato from "../styles/movie-info.module.css";
 import { API_URL } from "../app/(home)/page";
 
-async function getMovie(id:string){
+export async function getMovie(id: string) {
     const response = await fetch(`${API_URL}/${id}`);
     return response.json();
 }
@@ -13,7 +13,7 @@ export default async function MovieInfo({id}: {id:string}) {
             <img src={movie.poster_path} className={potato.poster} alt={movie.title}/>
             <div className={potato.info}>
                 <h1 className={potato.title}>{movie.title}</h1>
-                <h3> ⭐ {movie.vote_average.toFixed(1)}</h3>
+                <h3> ⭐ {movie.vote_average.toFixed()}</h3>
                 <p>{movie.overview}</p>
                 <a href={movie.homepage} target={"_blank"}>HomePage &rarr;</a>
             </div>
